70342abb9bb93edacfcfbf0434ae8e262383f45d
Sergi Piquer Cano <69960491+srgpqrcn@users.noreply.github.com>
2021-12-23 20:13:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 var show_button = document.getElementById("show_button");
+var stop_button = document.getElementById("stop_button");
 var location_data = document.getElementById("location_data");
+var map_URL_location = document.getElementById("map_URL");
 
-function geolocation(){
-    function geolocationSucces (posData){
-        location_data.innerHTML="latitud = " + posData.coords.latitude + "<br>longitud = " + posData.coords.longitude ;
-    };
-    /*navigator.geolocation.getCurrentPosition(position)*/
-    navigator.geolocation.watchPosition(geolocationSucces,geolocationError)
+function showMap(data){
+    var lat = 0;
+    var long = 0;
+    lat = data.coords.latitude;
+    long = data.coords.longitude;
+    map_URL_location.innerHTML="<a href=\"https://www.google.com/maps/search/?api=1&query="+lat+","+long+"\">VER EN MAPA</a>"
+}
+
+function geolocationSucces (posData){
+    location_data.innerHTML="latitud = " + posData.coords.latitude + "<br>longitud = " + posData.coords.longitude ;
+    showMap(posData);
+    console.log(posData);
 };
 
 function geolocationError(error){
     alert("geolocation does not work. Error code : "+error.code+" "+error.message)
 }
 
-
-// check for Geolocation support
-if (navigator.geolocation) {
-    console.log('Geolocation is supported!');
-    alert('Geolocation is supported!');
-}
-else {
-    console.log('Geolocation is not supported for this Browser/OS.');
-    alert('Geolocation is not supported for this Browser/OS.');
-    show_button.style.display="none"
+function stopGeolocation(){
+    navigator.geolocation.clearWatch(id);
+    alert(id+" geolocation was stopped")
+    location_data.innerHTML="";
+    map_URL_location.innerHTML="";
+    stop_button.style.display="none"
 }
 
+function geolocation(){
+    /*navigator.geolocation.getCurrentPosition(position)*/
+    if (navigator.geolocation){
+        stop_button.style.display="block"
+        id=navigator.geolocation.watchPosition(geolocationSucces,geolocationError)
+    }
+    else{
+        alert('Geolocation is not supported for this Browser/OS.');
+    }
 
+};
+
+stop_button.style.display="none"
+stop_button.onclick=stopGeolocation;
 show_button.onclick=geolocation;
