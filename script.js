2a5958549152755ecadd440edebc294a938bec56
Sergi Piquer Cano <69960491+srgpqrcn@users.noreply.github.com>
2021-12-23 20:30:30 +0100

    update

--- script.js
+++ script.js
@@ -9,17 +9,18 @@ function showMap(data){
     lat = data.coords.latitude;
     long = data.coords.longitude;
     map_URL_location.innerHTML="<a href=\"https://www.google.com/maps/search/?api=1&query="+lat+","+long+"\">VER EN MAPA</a>";
-}
+};
 
 function geolocationSucces (posData){
     location_data.innerHTML="latitud = " + posData.coords.latitude + "<br>longitud = " + posData.coords.longitude ;
+    alert("OK");
     showMap(posData);
     console.log(posData);
 };
 
 function geolocationError(error){
     alert("geolocation does not work. Error code : "+error.code+" "+error.message);
-}
+};
 
 function stopGeolocation(){
     navigator.geolocation.clearWatch(id);
@@ -27,13 +28,13 @@ function stopGeolocation(){
     location_data.innerHTML="";
     map_URL_location.innerHTML="";
     stop_button.style.display="none";
-}
+};
 
 function geolocation(){
     /*navigator.geolocation.getCurrentPosition(position)*/
     if (navigator.geolocation){
-        stop_button.style.display="block";
         id=navigator.geolocation.watchPosition(geolocationSucces,geolocationError);
+        stop_button.style.display="block";
     }
     else{
         alert('Geolocation is not supported for this Browser/OS.');
