eff8e21bae49c92541663bf07aa7fee0958b177f
Sergi Piquer Cano <69960491+srgpqrcn@users.noreply.github.com>
2023-11-16 21:19:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,20 @@
-var show_button = document.getElementById("show_button");
-var stop_button = document.getElementById("stop_button");
-var location_data = document.getElementById("location_data");
-var map_URL_location = document.getElementById("map_URL");
+var button = document.getElementById("titulo");
+var texto = document.getElementById("coord");
 
-function showMap(data){
-    var lat = 0;
-    var long = 0;
-    lat = data.coords.latitude;
-    long = data.coords.longitude;
-    map_URL_location.innerHTML="<a href=\"https://www.google.com/maps/search/?api=1&query="+lat+","+long+"\">VER EN MAPA</a>";
-};
-
-function geolocationSucces (posData){
-    location_data.innerHTML="latitud = " + posData.coords.latitude + "<br>longitud = " + posData.coords.longitude ;
-    showMap(posData);
-    console.log(posData);
-};
-
-function geolocationError(error){
-    alert("geolocation does not work. Error code : "+error.code+" "+error.message);
-};
-
-function stopGeolocation(){
-    navigator.geolocation.clearWatch(id);
-    alert(id+" geolocation was stopped");
-    location_data.innerHTML="";
-    map_URL_location.innerHTML="";
-    stop_button.style.display="none";
-};
-
-function geolocation(){
-    /*navigator.geolocation.getCurrentPosition(position)*/
-    if (navigator.geolocation){
-        id=navigator.geolocation.watchPosition(geolocationSucces,geolocationError);
-        stop_button.style.display="block";
+function accion(){
+    alert("START");
+    for(var i=0;i<10;i++){
+        console.log(i)
+        navigator.geolocation.getCurrentPosition(coordenadas);
     }
-    else{
-        alert('Geolocation is not supported for this Browser/OS.');
-    }
+    alert("STOP");
+}
+function coordenadas(pos){
+    texto.innerHTML="<h2>Latitude: " + pos.coords.latitude +
+    "<br>Longitude: " + pos.coords.longitude + "</h2>";
+    
+}
 
-};
 
-stop_button.style.display="none";
-stop_button.onclick=stopGeolocation;
-show_button.onclick=geolocation;
+
+button.ondblclick=accion;
