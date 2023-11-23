6445ef5146ff5328547455cc172b5c1d46e8f34d
Sergi Piquer Cano <69960491+srgpqrcn@users.noreply.github.com>
2023-11-24 00:19:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ var data = document.getElementById("dataArea");
 var bdHistorial =[];
 var eventId;
 
+let marker, circle, zoomed;
+var track;
+
 const map = L.map('map'); 
 // Initializes map
 
-map.setView([51.505, -0.09], 13); 
+map.setView([40.4167754,-3.7037902], 5); 
 // Sets initial coordinates and zoom level
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -16,10 +19,7 @@ attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap<
 }).addTo(map); 
 // Sets map data source and associates with map
 
-let marker, circle, zoomed;
-
 function iniciarTrack(){
-    //navigator.geolocation.getCurrentPosition(coordenadas);
     eventId = navigator.geolocation.watchPosition(coordenadas,errorUbic,{maximumAge:5000,timeout:5000});
 }
 
@@ -29,69 +29,52 @@ function stopTrack(){
 
 function crearHist(pos){
     bdHistorial.push([pos.coords.latitude,pos.coords.longitude]);
-    verHist();
-}
-
-function verHist(){
-    var auxText = "";
-    
-    for (var i=0;i<bdHistorial.length;i++){
-        pto=i+1;
-        auxText +="<h3>"+bdHistorial[i]+"</h3>";
-    }
-    //data.innerHTML=auxText;
 }
 
 function errorUbic(error){
     console.log(error.message);
+    if (error.code === 1) {
+        alert("Permitir ubicación");
+    } else {
+        //alert("¿Dónde estás?");
+    }
 }
 
 function coordenadas(pos){
     crearHist(pos);
+
+    var track = L.polyline(bdHistorial,{color:'red'}).addTo(map);
+    var startPos = L.circle([bdHistorial[0][0],bdHistorial[0][1]],{color:'red',radius:pos.coords.accuracy}).addTo(map);
+
     const lat = pos.coords.latitude;
     const lng = pos.coords.longitude;
     const accuracy = pos.coords.accuracy;
 
     if (marker) {
         map.removeLayer(marker);
-        map.removeLayer(circle);
+        //map.removeLayer(circle);
         }
-        // Removes any existing marker and circule (new ones about to be set)
+
+
+    // Removes any existing marker and circule (new ones about to be set)
         
-        marker = L.marker([lat, lng]).addTo(map);
-        circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
-        // Adds marker to the map and a circle for accuracy
+    marker = L.marker([lat,lng]).addTo(map);
         
-        if (!zoomed) {
-        zoomed = map.fitBounds(circle.getBounds()); 
-        }
-        // Set zoom to boundaries of accuracy circle
-        map.setView([lat, lng]);
-        // Set map focus to current user position
-}
+    //circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
+     // Adds marker to the map and a circle for accuracy
+        
+   
+    if (!zoomed) {
+    zoomed = map.fitBounds(track.getBounds());
+    }
 
-/*navigator.geolocation.watchPosition(success, error);
-function success(pos) {
-const lat = pos.coords.latitude;
-const lng = pos.coords.longitude;
-const accuracy = pos.coords.accuracy;
-if (marker) {
-map.removeLayer(marker);
-map.removeLayer(circle);
-}
-// Removes any existing marker and circule (new ones about to be set)
+    // Set zoom to boundaries of accuracy circle
+    map.setView([lat,lng]);
+    // Set map focus to current user position
 
-marker = L.marker([lat, lng]).addTo(map);
-circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
-// Adds marker to the map and a circle for accuracy
-
-if (!zoomed) {
-zoomed = map.fitBounds(circle.getBounds()); 
+    // zoom the map to the polyline
+    map.fitBounds(track.getBounds());
 }
-// Set zoom to boundaries of accuracy circle
-map.setView([lat, lng]);
-// Set map focus to current user position
-}*/
 
 
 buttonStart.onclick=iniciarTrack;
